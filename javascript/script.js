f5b6184d43c2782e9fb0c59cd0f8e58e4633d4d2
its-me-debk007 <debashish.joy@gmail.com>
2021-10-10 20:06:54 +0530

    Added dropping of egg from basket on pressing spacebar

--- javascript/script.js
+++ javascript/script.js
@@ -6,10 +6,17 @@ var basket3 = document.getElementById("basket3");
 basket.style.top = "3%";
 basket2.style.top = "41%";
 basket3.style.top = "81% ";
+egg.style.top = "38%";
 
-var flag = 0;
+var flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0, flag = 0;
 
 function move(obj){
+
+if(!flag)
+    obj.style.left = obj.offsetLeft + 5 + "px";
+else
+   obj.style.left = obj.offsetLeft - 5 + "px";
+
 
 if (obj.offsetLeft == (window.innerWidth - 80) )
     flag = 1;
@@ -17,33 +24,45 @@ if (obj.offsetLeft == (window.innerWidth - 80) )
 else if (obj.offsetLeft == 0)
     flag = 0;
 
-if(!flag)
-     obj.style.left = obj.offsetLeft + 5 + "px";
-else
-    obj.style.left = obj.offsetLeft - 5 + "px";
+// 2
+// if(!flag2)
+//     obj.style.left = obj.offsetLeft + 5 + "px";
+// else
+//    obj.style.left = obj.offsetLeft - 5 + "px";
 
-console.log(obj.offsetLeft);
+// if (obj.offsetLeft == (window.innerWidth - 80) )
+//     flag2 = 1;
+
+// else if (obj.offsetLeft == 0)
+//     flag2 = 0;
 
 }
 
-function move2(obj){
+setInterval(function(){ move(egg); }, 15);
+setInterval(function(){ move(basket) }, 15);
+setInterval(function(){ move(basket2) }, 15);
+setInterval(function(){ move(basket3) }, 15);
 
-    if (obj.offsetLeft == (window.innerWidth - 80) )
-        flag = 1;
-    
-    else if (obj.offsetLeft == 0)
-        flag = 0;
-    
-    if(!flag)
-         obj.style.left = obj.offsetLeft + 5 + "px";
-    else
-        obj.style.left = obj.offsetLeft - 5 + "px";
-    
-    console.log(obj.offsetLeft);
-    
+var keyStroke = null;
+window.addEventListener("keydown", (event) => { keyStroke = event.key; eggDrop(keyStroke);});
+
+var c1 = 0, c2 = 0, close = 0;
+
+function eggDrop(keyStroke){
+    if (keyStroke == " "){
+        // for(let i = 0; i<4; i++) egg.style.top = egg.offsetTop + 20 + "px";
+        var timer = setInterval(drop, 20);
+
+        function drop(){
+            egg.style.top = egg.offsetTop + 5 + "px";
+            if (egg.style.top > window.innerHeight)
+                clearInterval(timer);
+        }
+
+    }
+    if (egg.offsetTop > window.innerHeight){
+        alert("Game Over :(");
+        location.reload();
     }
 
-setInterval(function(){ move(egg); }, 2);
-setInterval(function(){ move(basket) }, 2);
-setInterval(function(){ move2(basket2) }, 8);
-setInterval(function(){ move(basket3) }, 16);
+}
