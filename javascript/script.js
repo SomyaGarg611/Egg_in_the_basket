7b237357f115ddde94583fbe66bc4b15b0cffc82
its-me-debk007 <debashish.joy@gmail.com>
2021-10-12 11:14:09 +0530

    The game will be continue forever

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,8 @@ var egg = document.getElementById("egg");
 var basket = document.getElementById("basket");
 var basket2 = document.getElementById("basket2");
 var basket3 = document.getElementById("basket3");
+var scoreboard = document.getElementById("scoreboard");
+
 
 basket.style.top = "3%";
 basket2.style.top = "41%";
@@ -39,7 +41,7 @@ function move2(basket){
         flag2 = 0;
     }
     
-var flag3 = 0;
+var flag3 = 0, score = 0;
 function move3(basket){
     if(!flag3)
         basket.style.left = basket.offsetLeft + 5 + "px";
@@ -54,64 +56,73 @@ function move3(basket){
         flag3 = 0;
     }
 
-var speed1 = Math.random()*20 + 10,
-            speed2 = Math.random()*20 + 15,
-                    speed3 = Math.random()*10 + 30;
+var speed1 = Math.random()*20 + 15,
+            speed2 = Math.random()*15 + 25,
+                    speed3 = Math.random()*10 + 35;
 
 var eggSpeed = setInterval(function(){ move(egg) }, speed1);
-setInterval(function(){ move(basket) }, speed1);
+setInterval(function() { move(basket) }, speed1);
 setInterval(function(){ move2(basket2) }, speed2);
 setInterval(function(){ move3(basket3) }, speed3);
 
 var keyStroke = null;
 window.addEventListener("keydown", (event) => { keyStroke = event.key; eggDrop(keyStroke);});
 
-var c = 0, c2 = 0, close = 0, angle = 0, level = 1, bsk;
+var c = 0, c2 = 0, close = 0, angle = 0, level = 1, press = 0, maxScore = 58;
 
 function eggDrop(keyStroke){
-if (keyStroke == " ")
+if (keyStroke == " " /*&& press == 0*/){
     var timer = setInterval(drop, 20);
-    
+    // press++;
+}
+
 function drop(){
     clearInterval(eggSpeed);
-    angle += 8;
-    egg.style.transform = "rotate(" + angle + "deg)";
-    // if(c != 13){
-    //     egg.style.top = egg.offsetTop - 5 + "px";
-    //     egg.style.left = egg.offsetLeft - 5 + "px";
-    //     c++;
-    // }
-    // else{
+    angle += 7;
+    egg.style.transform = `rotate(${angle}deg)`;
 
-    // if (c2 != 6){
-    // egg.style.left = egg.offsetLeft - 5 + "px";
-    // c2++;
-    // }
 egg.style.top = egg.offsetTop + 5 + "px";
 
-if (level == 1 && egg.offsetTop > basket2.offsetTop && egg.offsetLeft >= (basket2.offsetLeft - 25) && egg.offsetLeft <= (basket2.offsetLeft + 25)){
+if (level == 1 && egg.offsetTop > basket2.offsetTop && egg.offsetTop < (basket2.offsetTop + 45) && egg.offsetLeft >= (basket2.offsetLeft - 50) && egg.offsetLeft <= (basket2.offsetLeft + 50)){
+
 level = 2;
 clearInterval(timer);
 egg.style.transform = "rotate(0deg)";
-egg.style.left = basket2.style.left - 20 + "px";
+egg.style.left = basket2.style.left;
 eggSpeed = setInterval(function(){ move2(egg) }, speed2);
+egg.style.top = "39%";
+score++;
+if (score > maxScore) maxScore = score;
 }
 
-else if (level == 2 && egg.offsetTop > basket3.offsetTop && egg.offsetLeft >= (basket3.offsetLeft - 25) && egg.offsetLeft <= (basket3.offsetLeft + 25)){
+else if (level == 2 && egg.offsetTop > basket3.offsetTop && egg.offsetTop < (basket3.offsetTop + 45) && egg.offsetLeft >= (basket3.offsetLeft - 50) && egg.offsetLeft <= (basket3.offsetLeft + 50)){
 level = 3;
 clearInterval(timer);
 egg.style.transform = "rotate(0deg)";
-egg.style.left = basket3.style.left - 20 + "px";
+egg.style.left = basket3.style.left;
 eggSpeed = setInterval(function(){ move3(egg) }, speed3);
+egg.style.top = "79%";
+score++;
+// press = 0;
+if (score > maxScore) maxScore = score;
 
+setTimeout(function() {console.log("reached bottom")}, 5000);
+clearInterval(eggSpeed);
+egg.style.left = basket.style.left;
+egg.style.top = "0%";
+eggSpeed = setInterval(function(){ move(egg) }, speed1);
+level = 1;
+// press = 0;
 }
 
 if (egg.offsetTop > window.innerHeight){
-    alert("Game over :(");
+    alert(`                                             GAME OVER :(\n\n                                            YOUR SCORE : ${score}\n\n                                            MAX SCORE : ${maxScore}`);
     clearInterval(timer);
     clearInterval(eggSpeed);
     location.reload();
 }
-// }
 }
+
+
+
 }
