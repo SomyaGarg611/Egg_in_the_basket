8a8ddd25fc0412ada71554e207cae09c7cc47564
Somya Garg <somyagarg270@gmail.com>
2021-10-18 23:09:25 +0530

    Lives updated

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,36 @@ var egg = document.getElementById("egg");
 var basket = document.getElementById("basket");
 var basket2 = document.getElementById("basket2");
 var basket3 = document.getElementById("basket3");
+<<<<<<< Updated upstream
+=======
+var scoreboard = document.getElementById("scoreboard");
+var life = document.getElementById("lives");
+
+var flag1 = 0, flag2 = 0, flag3 = 0, score = 0, c = 0, c2 = 0, close = 0, angle = 0, level = 1, press = 0, maxScore = 72, lives = 5, count = 0, lvl = 0, lvl2 = lvl+1, lvl3 = lvl+2;
+
+var speed1 = Math.random()*25 + 10,
+                speed2 = Math.random()*15 + 20,
+                        speed3 = Math.random()*7 + 27;
+>>>>>>> Stashed changes
 
 basket.style.top = "3%";
 basket2.style.top = "41%";
 basket3.style.top = "81% ";
 egg.style.top = "0%";
 
+<<<<<<< Updated upstream
 var flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0, flag = 0;
+=======
+scoreboard.innerHTML = `SCORE : 0`;
+scoreboard.style.backgroundColor = "#9000ff";
+scoreboard.style.borderRadius = "50%";
+scoreboard.style.zIndex = "-2";
+
+life.innerHTML = `LIVES : 5`;
+life.style.backgroundColor = "#9000ff";
+life.style.borderRadius = "50%";
+life.style.zIndelife
+>>>>>>> Stashed changes
 
 function move(basket2){
 if(!flag)
